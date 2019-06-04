f8fd1bd8cb4d6e44e8f22608a28cf3d18f270240
Colin Scruggs <colin.scruggs@Dairycoms-MacBook-Pro.local>
2019-06-04 15:18:33 -0500

    Basic types

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
 "use strict";
-console.log("It works");
+// DATATYPES
+// string
+var myName = 'Colin'; // TS will automatically save the fact that myName should be a string type
+// myName = 21; will not compile
+// number (can be integer or float, no type differentiation)
+var myAge = 21;
+// myAge = '21'; will not compile
+// boolean
+var hasHobbies = true;
+// hasHobbies = 1; "truthiness" is not recognized when using different types (?)
+// assign types
+var myRealAge; // default type: any (vanilla JS variable)
+myRealAge = 21;
+// myRealAge = '21'; will not compile
+// array
+var hobbies = ["Producing music", "Skating"];
+hobbies = [100];
+// hobbies = 100; will not compile
+// tuples: can define types of tuples with specified types
+var address = ["Superstreet", 99];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue"; // 101
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor); // returns 100
+myColor = Color.Blue;
+console.log(myColor); // prints 101
+// any
+var car = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car);
--- app.ts
+++ app.ts
@@ -1,1 +1,42 @@
+// DATATYPES
 // string
+let myName = 'Colin'; // TS will automatically save the fact that myName should be a string type
+// myName = 21; will not compile
+
+// number (can be integer or float, no type differentiation)
+let myAge = 21;
+// myAge = '21'; will not compile
+
+// boolean
+let hasHobbies = true;
+// hasHobbies = 1; "truthiness" is not recognized when using different types (?)
+
+// assign types
+let myRealAge: number; // default type: any (vanilla JS variable)
+myRealAge = 21;
+// myRealAge = '21'; will not compile
+
+// array
+let hobbies: any[] = ["Producing music", "Skating"];
+hobbies = [100];
+// hobbies = 100; will not compile
+
+// tuples: can define types of tuples with specified types
+let address: [string, number] = ["Superstreet", 99];
+
+// enum
+enum Color {
+    Red, // 0
+    Green = 100, // 100
+    Blue // 101
+}
+let myColor: Color = Color.Green;
+console.log(myColor); // prints 100
+myColor = Color.Blue;
+console.log(myColor); // prints 101
+
+// any: most flexible variable type in TS
+let car: any = "BMW";
+console.log(car);
+car = {brand: "BMW", series: 3};
+console.log(car);
